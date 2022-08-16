2772874168ec9984abb102c26f9d0a8425534f79
Diana Laura Romero Feliciano <laura_feliciano25@hotmail.com>
2022-08-16 17:14:26 +0200

    ejercicio 14 sql resuelto

--- SQL/modulo-1-ejercicio-14-CTEs.sql
+++ SQL/modulo-1-ejercicio-14-CTEs.sql
@@ -37,29 +37,28 @@ FROM facturas;
 /*4. Contad el número de facturas por cliente
 Mejoremos la query anterior. En este caso queremos saber el número de facturas emitidas por cada cliente.*/
 
-WITH facturas (numero_factura, fecha_factura, codigo_cliente, compañia) AS
-(SELECT DISTINCT orders.order_id, orders.order_date, customers.customer_id, customers.company_name
+WITH facturas (numero_factura, codigo_cliente, compañia) AS
+(SELECT COUNT(orders.order_id), customers.customer_id, customers.company_name
 FROM orders
 INNER JOIN customers
 ON orders.customer_id = customers.customer_id
-GROUP BY orders.order_id, orders.order_date, customers.customer_id, customers.company_name)
-SELECT numero_factura, fecha_factura, codigo_cliente, compañia
+GROUP BY  customers.customer_id, customers.company_name)
+SELECT numero_factura, codigo_cliente, compañia
 FROM facturas;
 
 /*5. Cuál la cantidad media pedida de todos los productos ProductID.
-Necesitaréis extraet la suma de las cantidades por cada producto y calcular la media de todo */
+Necesitaréis extraet la suma de las cantidades por cada producto y calcular la media */
 
-CREATE TABLE pais_clientes
-WITH RECURSIVE pais_clientes (customer_id,company_name,pais) AS
-(
-SELECT customer_id, company_name, country 
-FROM customers
-WHERE country = 'Germany'
-UNION ALL
-SELECT c.customer_id, c.company_name, p.pais
-FROM pais_clientes AS p
-INNER JOIN customers AS c
-ON p.pais= c.country)
-SELECT * 
-FROM pais_clientes
-ORDER BY customer_id;
+WITH cantidad_media
+AS (
+    SELECT product_id,
+            AVG(quantity) AS cantidad_avg
+    FROM order_details
+    GROUP BY product_id 
+    )
+SELECT o.product_id,
+        SUM(o.quantity) AS cantidad,
+        c.cantidad_avg AS cantidad_media
+FROM order_details AS o
+JOIN cantidad_media AS c ON o.product_id = c.product_id
+GROUP BY product_id;
